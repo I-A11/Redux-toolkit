aba130a0b7d662d77cf5ba3ba65162216bfad527
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-03-14 11:09:35 +1100

    Create calculate reducer to modify total and amount in the cart.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import CartContainer from "./component/CartContainer";
 import Navbar from "./component/Navbar";
-
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
